d855ccaeae42ef581279aaeb648077ee43261abe
Maharo <rivomahefamaharo@gmail.com>
2020-10-10 13:36:19 +0300

    CameraGoUp

---
+++ GJPlusUnity/Assets/Scripts/camera/cameraControllerGoUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControllerGoUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector3(0, 1 ,-2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += new Vector3(0, 1 * Time.deltaTime,0);
+
+    }
+}
